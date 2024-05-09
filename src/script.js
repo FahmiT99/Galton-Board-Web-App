ad5415873dc248e1d82697791f71855dadb975f1
Abdulrazzak Tarabichi <newten1999uae@gmail.com>
2024-05-09 21:41:56 +0200

    ball ammount rangefield + locked the rangfield during animation

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ var stop = false;
 var pause = false; //stop execution
 var active = false; //animation is not active 
 var rows = 5;//Number of rows of pegs
-var balls = 100;
+var balls = 0;
 var cols = 2; // Number of columns of pegs it is a constant
 var gap = 250/rows; // Gap between pegs // standard value = 250/rows / update: dynamic size based on rows
 var radius = 50/rows; // Radius of pegs and balls //standard value = 50/rows  // same
@@ -23,6 +23,10 @@ var coordinates = []; //deepest level pegs coordinates
 var statsWatcher = {}; // contains the x postion of the buckets as keys and 
                             // an array of corresponding length and how often the ball entered the bucket
                             // as a second value x:[corresponding length, count]
+var newRowValue = rows; // temp save of the rows
+var newBallValue = balls;
+var probabilityRight = 50;
+var probabilityLeft = 50;
 
 
 
@@ -32,11 +36,14 @@ var statsWatcher = {}; // contains the x postion of the buckets as keys and
                             /*                                                      Animation
 ********************************************************************************************************************************** */
 
+function reloadCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        resetValues();
+        drawPegs();
+}
 
 
-
-function resetValues()
-{
+function resetValues() {
     pause = false; //stop execution
     active = false; //animation is not active 
     cols = 2; // Number of columns of pegs it is a constant
@@ -71,8 +78,7 @@ function drawPegs() {
             ctx.arc(x, y, radius, 0, Math.PI * 2);
             ctx.fill();
            // console.log("i is "+ i);   
-            if (i == rows - 1)
-            {
+            if (i == rows - 1) {
                 drawVerticalLine(x, y);
             }
         }
@@ -87,13 +93,9 @@ function drawHorizontalLine(x, y) {
     ctx.moveTo((canvas.width-gap) / 2 + gap * (-(rows-1)/ 2), y * 2.25);
     ctx.lineTo(x, y * 2.25);
     ctx.stroke();
-
 }
 
-
-
-function drawVerticalLine(x, y)
-{
+function drawVerticalLine(x, y){
     coordinates.push(x);
     ctx.lineWidth = 2;
     ctx.beginPath();
@@ -101,7 +103,6 @@ function drawVerticalLine(x, y)
     ctx.lineTo(x, y * 2.25);
     ctx.stroke();
 }
-
 
 function drawball(x_position = canvas.width / 2, y_position = gap) {
     ctx.fillStyle = "red";
@@ -111,9 +112,54 @@ function drawball(x_position = canvas.width / 2, y_position = gap) {
 
 }
 
-function wait(ms) 
-{
+function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function drawStats(x, y, n) {
+    console.log("drawSTATS() entered!");
+    ctx.lineWidth = gap-2;
+    let startingPoint = y * 2.25;
+    var length = (y + radius)/ n; 
+    ctx.beginPath();
+    const gradient = ctx.createLinearGradient(0,0,canvas.width, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("1.0", "red");
+    gradient.addColorStop("1.0", "red");
+    ctx.strokeStyle = gradient// "red";
+    
+    if (!statsWatcher.hasOwnProperty(x)) {
+
+        statsWatcher[x] = [length, 1];
+        ctx.moveTo(x, startingPoint);
+        console.log("n is " +n );
+        ctx.lineTo(x, startingPoint - length);       
+    } else {
+
+        ctx.moveTo(x,startingPoint - statsWatcher[x][0]);
+        statsWatcher[x][0]+=length;
+        statsWatcher[x][1]+=1;
+        console.log("statswateher has " + statsWatcher[x][1]);
+        console.log("n is " +n );
+        ctx.lineTo(x, startingPoint - statsWatcher[x][0]);
+    }
+    ctx.stroke(); 
+    ctx.strokeStyle = "black";
+}
+
+function drawStatsCount(x, y) {
+    y = y * 2.25 +gap/2;
+    let fontSize = gap*0.66;
+    ctx.font ="bold " +fontSize + "px Arial"; //
+    ctx.fillStyle = "red";
+    ctx.clearRect(x-gap/2, y-gap/2, gap, gap*1.5);
+    if (statsWatcher[x][1]<10)
+    ctx.fillText(statsWatcher[x][1], x-gap/6, y+gap/6);
+    else if(statsWatcher[x][1]<100) ctx.fillText(statsWatcher[x][1], x-gap/3, y+gap/6);
+    else {
+        ctx.font ="bold " + fontSize* 0.7 + "px Arial"; // 30% weniger größe bei Zahlen ab 100
+        ctx.fillText(statsWatcher[x][1], x-gap/3, y+gap/8);
+    }
 }
 
 // mainAnimationLoop is an asynchronous function that repeatedly calls the animate function
@@ -126,10 +172,9 @@ async function mainAnimationLoop() {
 }
 
 
-
 // createAnimation is a higher-order function that returns animateOneStep,
 // which uses closures to remember its state between calls so that we can pause the animation
-function createAnimation(n = 500, initial_n = 500) {  
+function createAnimation(n, initial_n ) {  
     var j = 0; // Platz zwischen pegs //standard Value: 0, also Mitte 
     var i = 1; //j gerade falls i ungerade und umgekehrt //Höhenebene
     var xPos = canvas.width / 2 - 0.5 * gap * j;
@@ -137,41 +182,38 @@ function createAnimation(n = 500, initial_n = 500) {
     var arr = [];
     var y = gap * rows;
 
-    return async function animateOneStep() {
-        
+    return async function animateOneStep() {  
         if (n < 0) {
             active = false; 
             statsWatcher = {}; 
             return;
         }
-        if (i <= rows+1) {
-           
-            
+
+        if (i <= rows+1) { 
             ctx.clearRect(0, 0, canvas.width, y + radius); //clear the upper half only
             drawball(xPos, yPos);
             drawPegs();
             await wait(speed);
             cols = 2;
-            console.log(rows+1);
-            if(i == rows+1) //the last level? 
-            {
+            console.log(rows);
+
+            if(i == rows+1) { //the last level? 
                 //use statLength to draw the how often a ball fits 
                 //between nth pegs. Implement and call drawStats() from here
                 drawStats(xPos, y , initial_n);  
-                drawStatsCount(xPos, y);     
-                //await wait(300);   
-                
+                drawStatsCount(xPos, y);             
             }
+
             xPos = canvas.width / 2 - 0.5 * gap * j;
             yPos = gap * i;
             var random = Math.random(); //Muss angepasst wegen Variationsm�glichkeit 3
             arr.push(random);           // Dazu muss die 0,5 in If-statement angepasst 
             if (random <= 0.5) {
                 j += 1;                 //going right 
-            }
-            else j -= 1;                 //going left
+            } else j -= 1;                 //going left
             i += 1;  
         }
+
         if (i > rows+1) {
             n -= 1;
             j = 0; 
@@ -180,70 +222,9 @@ function createAnimation(n = 500, initial_n = 500) {
             yPos = gap * i;
             arr = [];
             y = gap * rows;
-        }
-         
+        }  
     }
 }
-
-
-
-
-
-function drawStats(x, y, n)
-{
-    console.log("drawSTATS() entered!");
-    ctx.lineWidth = gap-2;
-    let startingPoint = y * 2.25;
-    var length = (y + radius)/ n; 
-    ctx.beginPath();
-
-
-    const gradient = ctx.createLinearGradient(0,0,canvas.width, 0);
-    gradient.addColorStop("0", "magenta");
-    gradient.addColorStop("1.0", "red");
-    gradient.addColorStop("1.0", "red");
-
-    ctx.strokeStyle = gradient// "red";
-    
-    if (!statsWatcher.hasOwnProperty(x))
-    {
-        statsWatcher[x] = [length, 1];
-        ctx.moveTo(x, startingPoint);
-        console.log("n is " +n );
-        ctx.lineTo(x, startingPoint - length);       
-    }
-    else
-    {
-        ctx.moveTo(x,startingPoint - statsWatcher[x][0]);
-        statsWatcher[x][0]+=length;
-        statsWatcher[x][1]+=1;
-        console.log("statswateher has " + statsWatcher[x][1]);
-        console.log("n is " +n );
-        ctx.lineTo(x, startingPoint - statsWatcher[x][0]);
-    }
-    ctx.stroke(); 
-    ctx.strokeStyle = "black";
-}
-
-function drawStatsCount(x, y)
-{
-    y = y * 2.25 +gap/2;
-    let fontSize = gap*0.66;
-    ctx.font ="bold " +fontSize + "px Arial"; //
-    ctx.fillStyle = "red";
-    ctx.clearRect(x-gap/2, y-gap/2, gap, gap*1.5);
-    if (statsWatcher[x][1]<10)
-    ctx.fillText(statsWatcher[x][1], x-gap/6, y+gap/6);
-    else if(statsWatcher[x][1]<100) ctx.fillText(statsWatcher[x][1], x-gap/3, y+gap/6);
-    else
-    {
-        ctx.font ="bold " + fontSize* 0.7 + "px Arial"; // 30% weniger größe bei Zahlen ab 100
-        ctx.fillText(statsWatcher[x][1], x-gap/3, y+gap/8);
-    }
-}
-
-
-
 
 
 
@@ -255,9 +236,8 @@ function drawStatsCount(x, y)
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
 var pauseButton = document.getElementById("pause");
-var rangeInput = document.getElementById("rangeInput"); //rows adjustment control
-var rangeValue = document.getElementById("rangeValue"); //current rows display
-var confirmButton = document.getElementById("confirm");
+var rowRangeInput = document.getElementById("rangeInput"); //rows adjustment control
+var rowRangeValue = document.getElementById("rangeValue"); //current rows display
 var speedRangeInput = document.getElementById("rangeInput2"); //speed adjustment control
 var speedRangeValue = document.getElementById("rangeValue2"); //current speed display
 var ballsAmountRangeInput = document.getElementById("rangeInput3"); //TODO
@@ -265,88 +245,65 @@ var ballsAmountRangeValue = document.getElementById("rangeValue3"); //TODO
 var probabilityRangeInput = document.getElementById("rangeInput4"); //TODO
 var probabilityRangeValue = document.getElementById("rangeValue4"); //TODO
 
-var newRowValue = rows; // temp save of the rows
-var newBallValue = balls;
-var probabilityRight = 0;
-var probabilityLeft = 0;
 
-ballsAmountRangeInput.addEventListener("input", () => 
-{
-    ballsAmountRangeValue.textContent = "Anzahl Bälle = " + ballsAmountRangeInput.value;
-    newBallValue = Number(ballsAmountRangeInput.value); 
+
+ballsAmountRangeInput.addEventListener("input", () => {
+    newBallValue = Number(ballsAmountRangeInput.value);
+    ballsAmountRangeValue.textContent = "Anzahl Bälle = " + Number(newBallValue+1);
+    balls = newBallValue; 
    
 });
 
-probabilityRangeInput.addEventListener("input", () => 
-    {
+probabilityRangeInput.addEventListener("input", () => {
         probabilityLeft = Number(probabilityRangeInput.value); 
         probabilityRight = 100 - probabilityLeft;
         probabilityRangeValue.innerHTML = "Wahrscheinlichkeit <br> links "+ probabilityLeft + " % | rechts "+ probabilityRight +" % ";
-        
-       
     });
 
-
-
 speedRangeInput.addEventListener("input", () => {
-    speed = 990 - Number(speedRangeInput.value) ;
+    speed = 990 - Number(speedRangeInput.value);
     console.log(speedRangeInput.value);
     speedRangeValue.textContent = "Fallgeschwindigkeit = " + Math.ceil((speedRangeInput.value*100)/1000) + "%"; //changed from floor to ceil
 });
 
-rangeInput.addEventListener("input", () => {
-    rangeValue.textContent = "Anzahl Reihen = " + rangeInput.value;
-    newRowValue = Number(rangeInput.value);
+rowRangeInput.addEventListener("input", () => {       
+        rowRangeValue.textContent = "Anzahl Reihen = " + rowRangeInput.value;
+        newRowValue = Number(rowRangeInput.value);
+        rows = newRowValue;
+        reloadCanvas();  
 });
-
-// alternative= integrate with start button, or do it dynamically like speed?
-confirmButton.addEventListener("click", () => {
-    if (!active)
-    {
-        //balls = newBallValue;
-        rows = newRowValue;  
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        resetValues();
-        drawPegs();
-    }
-});
-
-
 
 startButton.addEventListener("click", () => {
-
      if (pause) {
         // If the animation was paused, resume it
         pause = false;
-        mainAnimationLoop();
-        
+        mainAnimationLoop();   
     } else if (!active) {
-        // If the animation was not active, start it
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        cols= 2;
+        // If the animation was not active, start it 
+        resetValues();
         active = true;
-        animate = createAnimation(); 
+        rowRangeInput.disabled = true;
+        ballsAmountRangeInput.disabled = true;
+        probabilityRangeInput.disabled = true;
+        animate = createAnimation(balls,balls); 
         mainAnimationLoop();
     }
-
 });
+
 pauseButton.addEventListener("click",  () => {
-    if (active)
-    {
+    if (active) {
         pause = true;
     }
 });
-stopButton.addEventListener("click", async () => {
-    
-       // stop = true;
+
+stopButton.addEventListener("click", async () => {  
         active = false;
-        await wait(400);
+        rowRangeInput.disabled = false;
+        ballsAmountRangeInput.disabled = false;
+        probabilityRangeInput.disabled = false;
+        await wait(300);
         statsWatcher = {};
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        resetValues();
-        drawPegs();
-        return;
-    
+        reloadCanvas();
 });
 
 
