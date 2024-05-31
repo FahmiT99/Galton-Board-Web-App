d07ce381859e54904e76006f6b4291e5543eef88
fst99 <96962890+fst99@users.noreply.github.com>
2024-05-31 06:31:16 +0200

    added a function to reset db and plots, as well a scheduler that runs it every 24 hours after app startup

--- src/crud.py
+++ src/crud.py
@@ -1,9 +1,7 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import  Table, delete
 from models import *
 from schemas import *
-from database import engine
-import os
+import os, shutil
 
 """
 This module includes reusable CRUD functions
@@ -27,7 +25,6 @@ def get_user_data(db : Session, user_id:str):
    
 
 
-
 def create_group(db: Session, group_create: GroupCreate) -> bool:
 
     if not check_group_id_exists(db, group_create.group_id):
@@ -41,8 +38,6 @@ def create_group(db: Session, group_create: GroupCreate) -> bool:
         except:
             db.rollback()
             raise 
-        finally:
-            db.close()
         return True 
     else:
         # If the group ID already exists, return False
@@ -50,72 +45,42 @@ def create_group(db: Session, group_create: GroupCreate) -> bool:
 
 
 
+def save_data(db: Session, data_create: DataCreate) -> bool:
 
-def save_data(db: Session, data_create: DataCreate):
-
-    new_data = Data(**data_create.model_dump())
-    try:
-        db.add(new_data)          
-        db.commit()              
-        db.refresh(new_data)      
-    except:
-        db.rollback()             
-        raise                   
-    finally:
-        db.close()               
+    if  check_group_id_exists(db, data_create.group_id):
+        new_data = Data(**data_create.model_dump())
+        try:
+            db.add(new_data)          
+            db.commit()              
+            db.refresh(new_data)      
+        except:
+            db.rollback()             
+            raise
+        return True
+    else:
+        return False                                 
 
 
     
-def reset_data(db : Session):
-    table1 = Table("data", Base.metadata, autoload_with=engine)
-    table2 = Table("groups", Base.metadata, autoload_with=engine)
-    query1 = delete(table1)
-    query2 = delete(table2)
+def reset_db_entries(db : Session):
     try:
-        db.execute(query1)
-        db.execute(query2)
+        db.query(Data).delete()
+        db.query(Group).delete()
+        db.query(User).delete()
         db.commit()
     except:
         db.rollback()
         raise
     finally:
         db.close()
+    
 
 
+def reset_data(db: Session):
 
-# delete rows of a group after a certain time
-def delete_group_data(db : Session, group_id):
-    try:
-        query = delete(Data).where(Data.group_id == group_id)
-        query_2 = delete(Group).where(Group.group_id == group_id)
-        db.execute(query)
-        db.commit()
-        db.execute(query_2)
-        db.commit()
-    except:
-        db.rollback()
-        raise
-    finally:
-        db.close()    
-
-
-def cleanup_plots_and_db(db: Session):
-
+    reset_db_entries(db)
     plot_dir = "frontend/plots"
-    plot_files = [file for file in os.listdir(plot_dir) if os.path.isfile(os.path.join(plot_dir, file))]
-
-    if len(plot_files) > 100:
-        # Sort files by modification time (oldest first)
-        plot_files.sort(key=lambda x: os.path.getmtime(os.path.join(plot_dir, x)))
-
-        # Identify the oldest group_id and remove corresponding files and database rows
-        oldest_file = plot_files[0]
-        oldest_group_id = oldest_file.split('_')[0]
-
-        # Remove all files with the oldest group_id
-        for file in plot_files:
-            if file.startswith(f"{oldest_group_id}_"):
-                os.remove(os.path.join(plot_dir, file))
-
-        # Remove corresponding rows from the database
-        delete_group_data(db, oldest_group_id) 
+    # Remove the directory if it exists
+    if os.path.exists(plot_dir):
+        shutil.rmtree(plot_dir)
+        os.makedirs(plot_dir)
